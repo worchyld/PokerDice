37bfb0d8c48efc6210553938ad20cdaf9bf6331a
AD <silentflute.ad@gmail.com>
2024-09-05 15:02:18 +0100

    Trying Cursor ai to help me write this

--- PokerDice/PokerDice/PokerDice.swift
+++ PokerDice/PokerDice/PokerDice.swift
@@ -8,16 +8,8 @@
 import Foundation
 import GameplayKit
 
-enum PokerDice: String, CaseIterable {
-    case nine = "9"
-    case ten = "10"
-    case jack = "J"
-    case queen = "Q"
-    case king = "K"
-    case ace = "A"
-}
 
-enum PokerHandRank: Int {
+enum PokerHandRank: Int, CaseIterable {
     case fiveOfAKind = 7
     case fourOfAKind = 6
     case fullHouse = 5
@@ -27,4 +19,55 @@ enum PokerHandRank: Int {
     case onePair = 1
     case highCard = 0
 }
+
+func evaluatePokerHand(_ hand: [PokerDice]) -> PokerHandRank {
+    guard hand.count == 5 else {
+        fatalError("A poker hand must contain exactly 5 dice")
+    }
+    
+    let diceCounts = Dictionary(hand.map { ($0, 1) }, uniquingKeysWith: +)
+    let sortedCounts = diceCounts.values.sorted(by: >)
+    
+    // Check for Five of a Kind
+    if sortedCounts[0] == 5 {
+        return .fiveOfAKind
+    }
+    
+    // Check for Four of a Kind
+    if sortedCounts[0] == 4 {
+        return .fourOfAKind
+    }
+    
+    // Check for Full House
+    if sortedCounts[0] == 3 && sortedCounts[1] == 2 {
+        return .fullHouse
+    }
+    
+    // Check for Straight
+    let sortedHand = hand.sorted { $0.rawValue < $1.rawValue }
+    if Set(sortedHand) == Set(PokerDice.allCases.prefix(5)) || 
+       Set(sortedHand) == Set(PokerDice.allCases.suffix(5)) {
+        return .straight
+    }
+    
+    // Check for Three of a Kind
+    if sortedCounts[0] == 3 {
+        return .threeOfAKind
+    }
+    
+    // Check for Two Pair
+    if sortedCounts[0] == 2 && sortedCounts[1] == 2 {
+        return .twoPair
+    }
+    
+    // Check for One Pair
+    if sortedCounts[0] == 2 {
+        return .onePair
+    }
+    
+    // If none of the above, it's a High Card
+    return .highCard
+}
+
+
 
--- PokerDice/PokerDiceTests/PokerDiceTests.swift
+++ PokerDice/PokerDiceTests/PokerDiceTests.swift
@@ -18,4 +18,44 @@ final class PokerDiceTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testFiveOfAKind() {
+        let hand = PokerDiceHand(dice: [1, 1, 1, 1, 1])
+        XCTAssertEqual(hand.evaluateHand(), .fiveOfAKind)
+    }
+
+    func testFourOfAKind() {
+        let hand = PokerDiceHand(dice: [2, 2, 2, 2, 5])
+        XCTAssertEqual(hand.evaluateHand(), .fourOfAKind)
+    }
+
+    func testFullHouse() {
+        let hand = PokerDiceHand(dice: [3, 3, 3, 4, 4])
+        XCTAssertEqual(hand.evaluateHand(), .fullHouse)
+    }
+
+    func testStraight() {
+        let hand = PokerDiceHand(dice: [1, 2, 3, 4, 5])
+        XCTAssertEqual(hand.evaluateHand(), .straight)
+    }
+
+    func testThreeOfAKind() {
+        let hand = PokerDiceHand(dice: [6, 6, 6, 2, 3])
+        XCTAssertEqual(hand.evaluateHand(), .threeOfAKind)
+    }
+
+    func testTwoPair() {
+        let hand = PokerDiceHand(dice: [1, 1, 4, 4, 5])
+        XCTAssertEqual(hand.evaluateHand(), .twoPair)
+    }
+
+    func testOnePair() {
+        let hand = PokerDiceHand(dice: [2, 2, 3, 4, 6])
+        XCTAssertEqual(hand.evaluateHand(), .onePair)
+    }
+
+    func testNoPair() {
+        let hand = PokerDiceHand(dice: [1, 3, 4, 5, 6])
+        XCTAssertEqual(hand.evaluateHand(), .noPair)
+    }
+
 }
